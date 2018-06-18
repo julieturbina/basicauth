a85e279c4e184d2fd5544883611dbb10af3e57f6
Alan Cardero <alan.cardero@gmail.com>
2018-06-18 19:17:28 -0400

    adding the files for basic auth

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const app = express();
 
 mongoose.Promise = Promise;
 mongoose
@@ -21,8 +22,6 @@ mongoose
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
-const app = express();
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -37,22 +36,17 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
+//Routes
+const authRoutes = require('./routes/auth-routes');
+app.use('/', authRoutes);
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
-
-
-
-const index = require('./routes/index');
-app.use('/', index);
-
 
 module.exports = app;
